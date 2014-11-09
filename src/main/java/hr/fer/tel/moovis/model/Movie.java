fb879fb2d94ff21b8823c4c1a86d8a66e7851dba
Tomislav <smrecki.tomislav@gmail.com>
2014-11-09 15:52:54 +0100

    Jednostavan model (tek toliko da imamo nekaj za početak)

---
+++ src/main/java/hr/fer/tel/moovis/model/Movie.java
@@ -0,0 +1,60 @@
+package hr.fer.tel.moovis.model;
+
+/**
+ * Created by tomislaf on 9.11.2014..
+ */
+public class Movie {
+
+    private String title;
+    private String description;
+    private String imdbId;
+    private String tmdbId;
+    private String rottenId;
+
+    public Movie(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(String tmdbId) {
+        this.tmdbId = tmdbId;
+    }
+
+    public String getRottenId() {
+        return rottenId;
+    }
+
+    public void setRottenId(String rottenId) {
+        this.rottenId = rottenId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/hr/fer/tel/moovis/model/User.java
@@ -0,0 +1,61 @@
+package hr.fer.tel.moovis.model;
+
+import java.util.List;
+
+/**
+ * Created by tomislaf on 9.11.2014..
+ */
+public class User {
+
+    private long userId;
+    private String name;
+    private String surname;
+    private String facebookAccessToken;
+    private String twitterAccessToken;
+    private List<Movie> likedMovies;
+
+    public User(String name, String surname){
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFacebookAccessToken() {
+        return facebookAccessToken;
+    }
+
+    public void setFacebookAccessToken(String facebookAccessToken) {
+        this.facebookAccessToken = facebookAccessToken;
+    }
+
+    public String getTwitterAccessToken() {
+        return twitterAccessToken;
+    }
+
+    public void setTwitterAccessToken(String twitterAccessToken) {
+        this.twitterAccessToken = twitterAccessToken;
+    }
+
+    public List<Movie> getLikedMovies() {
+        return likedMovies;
+    }
+
+    public void setLikedMovies(List<Movie> likedMovies) {
+        this.likedMovies = likedMovies;
+    }
+}
