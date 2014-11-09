2586a4834280c02f026cde6a686265e1cb832280
filippm <filip.cajko@gmail.com>
2014-11-09 22:38:00 +0100

    aaa

--- src/main/java/hr/fer/tel/moovis/searchers/TMDBSearch.java
+++ src/main/java/hr/fer/tel/moovis/searchers/TMDBSearch.java
@@ -1,8 +1,9 @@
 package hr.fer.tel.moovis.searchers;
 
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 import com.omertron.themoviedbapi.MovieDbException;
 import com.omertron.themoviedbapi.TheMovieDbApi;
+import com.omertron.themoviedbapi.model.MovieDb;
 
 import java.net.UnknownHostException;
 
@@ -10,14 +11,15 @@ import java.net.UnknownHostException;
  * Created by filippm on 09.11.14..
  */
 public class TMDBSearch implements Runnable {
-
+    private static final String DB_NAME = "moovis";
+    private static final String MY_QUEUE = "THDBSearchQueue";
     private static final String API_KEY = "5086b39f4b96483187ec864955e4da88";
     private static final int SLEEP_TIME = 100 * 60;
-    private TheMovieDbApi dbApi;
+    private TheMovieDbApi theMovieDbApi;
     private MongoClient mongo;
 
     public TMDBSearch() throws MovieDbException, UnknownHostException {
-        dbApi = new TheMovieDbApi(API_KEY);
+        theMovieDbApi = new TheMovieDbApi(API_KEY);
 
         // Since 2.10.0, uses MongoClient
         mongo = new MongoClient("localhost", 27017);
@@ -25,6 +27,8 @@ public class TMDBSearch implements Runnable {
 
 
     public void run() {
+        System.out.println(Thread.currentThread().getId());
+
         while (true) {
             searchProcess();
             try {
@@ -36,7 +40,41 @@ public class TMDBSearch implements Runnable {
     }
 
     private void searchProcess() {
+        DB db = mongo.getDB(DB_NAME);
+        DBCollection table = db.getCollection(MY_QUEUE);
+
+        DBCursor cursor = table.find();
+        DBObject obj;
+
+        try {
+            while (cursor.hasNext()) {
+                //Dohvati iz reda
+                obj = cursor.next();
+                table.remove(obj);
+                System.out.println(obj);
+                String movieKey = obj.get("movieKey").toString();
+                String movieName = obj.get("name").toString();
+                String movieYear = obj.get("year").toString();
+                System.out.println(movieKey);
+                System.out.println(movieName);
+                System.out.println(movieYear);
+
+                //Obrada uz TMDB api
+                try {
+                    MovieDb movie = theMovieDbApi.searchMovie(movieName, Integer.parseInt(movieYear), null, false, 0).getResults().get(0);
+                    System.out.println(movie);
+                } catch (MovieDbException e) {
+                    continue;
+                }
+            }
+        } finally {
+            cursor.close();
+        }
+    }
 
 
+    public static void main(String[] args) throws Exception {
+        System.out.println(Thread.currentThread().getId());
+        new Thread(new TMDBSearch()).start();
     }
 }
