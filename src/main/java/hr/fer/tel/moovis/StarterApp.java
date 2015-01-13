8ab08fc5a1e38585bee2ab0831ac6ed96e432532
filippm <filip.cajko@gmail.com>
2015-01-13 18:46:56 +0100

    aaa

--- src/main/java/hr/fer/tel/moovis/StarterApp.java
+++ src/main/java/hr/fer/tel/moovis/StarterApp.java
@@ -1,18 +1,24 @@
 package hr.fer.tel.moovis;
 
+import hr.fer.tel.moovis.dao.ApplicationUserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by filippm on 10.11.14..
  */
 @EnableAutoConfiguration
 @ComponentScan
+@Configuration
 public class StarterApp extends SpringBootServletInitializer {
+	@Autowired
+	ApplicationUserRepository rep;
 
 	@Override
 	protected SpringApplicationBuilder configure(
@@ -21,6 +27,8 @@ public class StarterApp extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Starting...");
 		SpringApplication.run(StarterApp.class, args);
+		System.out.println("Started");
 	}
 }
