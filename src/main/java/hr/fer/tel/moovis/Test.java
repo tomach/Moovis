26fac4467b6bd4b92aa10f22fe8879076f152d13
filippm <filip.cajko@gmail.com>
2015-01-15 17:38:31 +0100

    a

--- src/main/java/hr/fer/tel/moovis/Test.java
+++ src/main/java/hr/fer/tel/moovis/Test.java
@@ -7,18 +7,19 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.omertron.themoviedbapi.MovieDbException;
+import com.omertron.themoviedbapi.TheMovieDbApi;
 
 public class Test {
+	private static final String API_KEY = "5086b39f4b96483187ec864955e4da88";
 
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws UnknownHostException,
+			MovieDbException {
 		// TODO Auto-generated method stub
-		MongoClient mongo = new MongoClient("localhost", 27017);
-		DB db = mongo.getDB("moovis");
-		DBCollection movies = db.getCollection("movies");
+		TheMovieDbApi theMovieDbApi = new TheMovieDbApi(API_KEY);
 
-		DBObject movie = movies.findOne(new BasicDBObject("movieKey",
-				"Interstellar"));
-		System.out.println(movie.containsField("tmdb.id"));
+		System.out.println(theMovieDbApi.getMovieImages(2348, null, "")
+				.getResults().get(0).getFilePath());
+
 	}
-
 }
